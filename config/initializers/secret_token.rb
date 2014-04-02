81a63a502b47a25cdc4f73da019de165af06f1cd
krillen <okulby@yahoo.com>
2014-04-02 16:49:40 -0700

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Wikisass::Application.config.secret_token = '2d50539fc32b8a73edc33fb1ae5a8a8ffb33a9ce22580e6c7c7a07ec9dc028c7cd95ceb97399ca99e73a51ab0c72a929069fe444e8146448db2f21b5a262f850'
