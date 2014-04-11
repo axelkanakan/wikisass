1d84db8630933492aba4ed50c083f9e20ef65935
krillen <okulby@yahoo.com>
2014-04-11 02:16:53 -0700

    Added Markdown

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,3 +1,11 @@
 module ApplicationHelper
 	include FoundationRailsHelper::FlashHelper
+
+
+	def markdown(text)
+		renderer = Redcarpet::Render::HTML.new
+		extensions = {fenced_code_blocks: true}
+		redcarpet = Redcarpet::Markdown.new(renderer, extensions)
+		(redcarpet.render text).html_safe
+	end
 end
