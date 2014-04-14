46dd57abf6c75da0df47dbd76d8434f3366fd019
krillen <okulby@yahoo.com>
2014-04-13 19:41:58 -0700

    Sub

--- app/controllers/subcriptions_controller.rb
+++
@@ -1,7 +0,0 @@
-class SubcriptionsController < ApplicationController
-  def new
-  end
-
-  def create
-  end
-end
--- config/routes.rb
+++ config/routes.rb
@@ -10,6 +10,7 @@ Wikisass::Application.routes.draw do
 
   devise_for :users
   
+  
   root :to => 'welcome#index'  
 
 end
