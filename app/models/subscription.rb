6bd0fff59ae6e45e7ccc8951d0a58dcaa0a1204b
krillen <okulby@yahoo.com>
2014-04-14 03:07:41 -0700

    Stripe part I

--- app/models/subscription.rb
+++ app/models/subscription.rb
@@ -2,5 +2,4 @@ class Subscription < ActiveRecord::Base
   belongs_to :user
   belongs_to :plan
   attr_accessible :email, :stripe_customer_token
- 
 end
---
+++ config/initializers/stripe.rb
@@ -0,0 +1,2 @@
+Stripe.api_key = "sk_test_fJodJbPgGi0KM42EvCgMAufO"
+STRIPE_PUBLIC_KEY = "pk_test_X413adS5vWgp1zB13tiGEp6S"
