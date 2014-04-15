d16d720e7b10991ed46a23719ed0fb74c556f2cf
krillen <okulby@yahoo.com>
2014-04-15 04:08:15 -0700

    Stripe implemented

--- app/controllers/subscriptions_controller.rb
+++ app/controllers/subscriptions_controller.rb
@@ -4,12 +4,20 @@ class SubscriptionsController < ApplicationController
   end
 
   def create
-  	@subscription = Subscription.new(params[:subscription])
-  	if @subscription.save
-  		redirect_to @subscription, :notice => "Thank you for subscribing!"
-  	else
-  		render :new
-  	end
+    @subscription = current_user.build_subscription(params[:subscription])    
+    if @subscription.save_with_payment
+       current_user.update_attribute(:role)
+       redirect_to root_path, :notice => "Thank you for subscribing!"
+    else
+      flash[:alert] = "Error"
+      render :new
+    end
   end
 
+  def show
+    @subscription = Subscription.find(params[:id])
+  end
+  
+  def destroy
+  end
 end
--- app/models/subscription.rb
+++ app/models/subscription.rb
@@ -1,5 +1,20 @@
 class Subscription < ActiveRecord::Base
   belongs_to :user
   belongs_to :plan
-  attr_accessible :email, :stripe_customer_token
+  attr_accessible :email, :stripe_customer_token, :plan_id, :stripe_card_token
+  validates_presence_of :plan_id
+  attr_accessor :stripe_card_token
+
+
+  def save_with_payment
+    if valid?
+      customer = Stripe::Customer.create(description: email, plan: plan_id, card: stripe_card_token)
+      self.stripe_customer_token = customer.id
+      save!
+    end
+    rescue Stripe::InvalidRequestError => e
+      logger.error "Stripe error while creating customer: #{e.message}"
+      errors.add :base, "There was a problem with your credit card."
+      false
+  end
 end
